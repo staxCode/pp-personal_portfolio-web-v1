a11a8be03e38389b3919fd991841c086f5e8d5d7
Sting <sik77mo@gmail.com>
2023-01-30 13:32:47 -0500

    footer update

--- components/Footer.js
+++ components/Footer.js
@@ -2,12 +2,12 @@ const Footer = () => {
   return (
     <footer className="p-4 bg-white md:px-6 md:py-8 dark:bg-gray-900">
       <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
-      <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+      <span className="flex justify-center items-strech text-sm text-gray-500 dark:text-gray-400">
         Developed by
-        <a href="https://github.com/staxCode" className="ml-1 hover:underline">
-          @staxCode
+        <a href="https://github.com/staxCode" className="mx-1 hover:underline font-semibold">
+          @staxCode.
         </a>
-        . ❤
+         <i class="ri-ghost-smile-line ri-lg self-center"></i>
       </span>
     </footer>
   );
